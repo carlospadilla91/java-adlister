ac767fda9035fd8b265970254f89f6c548f530af
Carlos Padilla <mr.carlos.padilla@gmail.com>
2021-04-12 15:42:28 -0500

    feat: completed javabeans exercise

---
+++ src/main/java/Album.java
@@ -0,0 +1,62 @@
+import java.io.Serializable;
+import java.util.Date;
+
+public class Album implements Serializable {
+    private int id;
+    private String artist;
+    private String albumName;
+    private int releaseDate;
+    private long sales;
+    private String genre;
+
+    public Album() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public void setAlbumName(String albumName) {
+        this.albumName = albumName;
+    }
+
+    public int getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(int releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public long getSales() {
+        return sales;
+    }
+
+    public void setSales(long sales) {
+        this.sales = sales;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
---
+++ src/main/java/BeanTest.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class BeanTest {
+
+    public static void main(String[] args) {
+
+        Album album = new Album();
+
+        Author author = new Author();
+        author.setFirstName("Carlos");
+        author.setLastName("Padilla");
+        author.setId(1);
+
+        Quote quote1 = new Quote();
+        quote1.setContent("To be or not to be.");
+        Quote quote2 = new Quote();
+        quote2.setContent("Do or do not. There is no try.");
+        Quote quote3 = new Quote();
+        quote3.setContent("You miss 100% of the shots you don't take.");
+
+
+        List<Quote> quotes = new ArrayList<>();
+        quotes.add(quote1);
+        quotes.add(quote2);
+        quotes.add(quote3);
+
+        for (Quote quote : quotes) {
+            System.out.println(quote.getContent());
+        }
+    }
+}
